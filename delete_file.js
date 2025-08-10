4ab7ce2172ec6b590390b941f5b691aa5b884437
Olsi Gjeci <olsi.gjeci@snyk.io>
2025-08-10 08:12:40 +0100

    your file id

--- delete_file.js
+++ delete_file.js
@@ -2,7 +2,7 @@ import openai from "./openaiConfig.js";
 
 async function main(){
 
-    const deletedFile = await openai.files.delete("file-LMXJwBFxJAFEXfwmpCWtS7");
+    const deletedFile = await openai.files.delete("your_file_id");
     console.log(deletedFile);
 
 }
--- retrieve_file.js
+++ retrieve_file.js
@@ -1,7 +1,7 @@
 import openai from "./openaiConfig.js"
 
 async function main() {
-    const file = await openai.files.retrieve("file-LMXJwBFxJAFEXfwmpCWtS7")
+    const file = await openai.files.retrieve("your_file_id")
     console.log(file)
 }
 
